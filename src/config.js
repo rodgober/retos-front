9122bce3bea353b3fdacf4dcbf9b8528177d145f
Rodrigo Bernardino <rodgober7@hotmail.com>
2024-09-25 19:10:52 -0600

    Apuntando al back productivo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl: 'http://localhost:5000'
+    //apiBaseUrl: 'http://localhost:5000'
+    apiBaseUrl: 'https://retos-back.onrender.com'
   };
   
   export default config;
