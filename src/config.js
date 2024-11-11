debcc2dc01e861c740f7f6d41664ba27f8cbb225
Rodrigo Bernardino <rodgober7@hotmail.com>
2024-11-11 16:43:44 -0600

    Versión 0.91

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
-  apiBaseUrl: window.location.hostname === 'localhost'
+  apiBaseUrl: 'https://retos-back.onrender.com'
+ /* apiBaseUrl: window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
-    : 'http://192.168.100.35:5000' //apiBaseUrl: 'https://retos-back.onrender.com'
+    : 'http://192.168.100.35:5000' *///
 };
   
 
