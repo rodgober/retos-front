1a478b3f875ee3f3f7c018fcdaa01f234f2e6dc5
Rodrigo Bernardino <rodgober7@hotmail.com>
2024-11-14 11:39:33 -0600

    Se agregó la opción de Contact

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ import { version } from "react";
 
 const config = {
   apiBaseUrl: 'https://retos-back.onrender.com',
-  /*apiBaseUrl: window.location.hostname === 'localhost'
+/*  apiBaseUrl: window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
-    : 'http://192.168.100.35:5000',*/
-  version: "1.2"
+    : 'http://192.168.100.35:5000', */
+  version: "1.3"
 };
   
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import config from '../config';
+import './Contact.css';
 
 function Contact() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post(config.apiBaseUrl + '/api/contacto/mensaje', { email, message });
+        setStatus("Mensaje enviado con éxito.");
+        setEmail('');
+        setMessage('');
+    } catch (error) {
+        setStatus("Hubo un error al enviar el mensaje.");
+    }
+};
   return (
     <div className="container-contact">
-      <p>Para ponerte en contacto conmigo, ya sea para corregir la redacción o resultado de un reto, para proponer nuevos retos, para adaptar o agregar nuevas funcionalidades o simplemente para manter una conversación te comparto mi correo electrónico:</p>
-      <p>Si quieres algo adicional, mándame un correo.</p>
+      <h2 className="contact-form-title">Contáctanos</h2>
+      <p>Para ponerte en contacto con nosotros, ya sea para corregir la redacción o resultado de un reto, 
+        para proponer nuevos retos, adaptar o agregar nuevas funcionalidades o simplemente para manter una conversación, 
+        envíanos un mensaje.</p>
+      <form className="contact-form" onSubmit={handleSubmit}>
+            <label>
+                Escribe tu correo:
+                <input 
+                    type="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    required 
+                />
+            </label>
+            <label>
+                Escribe tu comentario:
+                <textarea 
+                    value={message} 
+                    onChange={(e) => setMessage(e.target.value)} 
+                    required 
+                />
+            </label>
+            <button type="submit">Enviar</button>
+            <p>{status}</p>
+        </form>
     </div>
   );
 }
