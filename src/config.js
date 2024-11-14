0e7cba4de406595f82fe2ddcd5b0cb430ae22fb8
Rodrigo Bernardino <rodgober7@hotmail.com>
2024-11-14 13:35:06 -0600

    Recuperar contraseña con direccion del back correcta

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { version } from "react";
 
 const config = {
-//  apiBaseUrl: 'https://retos-back.onrender.com',
+  apiBaseUrl: 'https://retos-back.onrender.com',
 /*  apiBaseUrl: window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
     : 'http://192.168.100.35:5000', */
