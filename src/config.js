d8aac24716d9fd61a7ab4620d030c8aef3bdc25a
Rodrigo Bernardino <rodgober7@hotmail.com>
2024-11-15 10:54:13 -0600

    Error del error de la coma de versión

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { version } from "react";
 
 const config = {
   apiBaseUrl: 'https://retos-back.onrender.com',
-  apiBaseUrl: window.location.hostname === 'localhost',
-/*    ? 'http://localhost:5000'
+/*  apiBaseUrl: window.location.hostname === 'localhost',
+    ? 'http://localhost:5000'
     : 'http://192.168.100.35:5000', */
   version: "1.5"
 };
